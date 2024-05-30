93509bd44d6758faf785cd855b454dc62bcc8353
sssssuihacker <mohamedhmdh9@gmail.com>
2024-05-30 16:43:43 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'token': 'MTI0NTY2NTIzMzg3MjA5MzI1Nw.Gd1ljB.DLop6KqTPAnVK1g9wgMLqBW4-ke60OH-NOYiTA', // Your bot token, activate all bot intents in discord developer portal.
+    'clientid': '1245665233872093257', // Your discord bot id.
+    'guildid': '1016464719739101266', // Id of a private server to load the bot's slash commands.
+    'ownerid': ['970443293613760602'], // Add discord ids to the array to access the commands.
+    'webhook': 'https://discordapp.com/api/webhooks/1245666709432438836/8Wbk9nYPQQRN-TJfzaenr4GDjW2PS15eVuflpvpex-gyZWc5ifELy8PVWG8oGOjrEqNi' // Your discord webhook url.
+}
